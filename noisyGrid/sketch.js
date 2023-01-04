28df9631ead3b1fca52c688eaeb6a7d2a86ca2da
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-01-04 03:54:31 +0300

    Step 6 and 7 for noisyGrid

--- noisyGrid/sketch.js
+++ noisyGrid/sketch.js
@@ -2,25 +2,22 @@
 
 /* p5js gloabl functions and variable for ESLint to ignore: */
 /* global color, background, createCanvas, frameCount, fill, noise, noStroke, mouseX, width */
-/* global rect, lerpColor, vertex, rotate, beginShape, endShape, translate, CLOSE, map,  */
+/* global rect, lerpColor, rotate, translate, map, angleMode, stroke, strokeWeight, DEGREES, 
+          push, pop, line */
 
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "setup|draw|keyPressed|keyTyped|mouseReleased" }] */
 
 const stepSize = 20;
 
 function colorGrid() {
-  const green = color(0, 255, 0);
-  const red = color(255, 0, 0);
-  // const factor = 0.01;
-
   for (let x = 0; x < 25; x += 1) {
     for (let y = 0; y < 25; y += 1) {
-      // Step 3: Use the mouse position to control the noise factor
       const factor = 0.02;
+      // Step 3: Use the mouse position to control the noise factor
       const speedFactor = map(width - mouseX, 0, width, 0.01, 0.05, true);
 
       const t = noise(x * factor, y * factor, frameCount * speedFactor);
-      const c = lerpColor(green, red, t);
+      const c = lerpColor(color('green'), color('red'), t);
 
       fill(c);
       noStroke();
@@ -30,6 +27,7 @@ function colorGrid() {
 }
 
 function compassGrid() {
+  // Step 4: Grid of compasses
   for (let x = 0; x < 25; x += 1) {
     for (let y = 0; y < 25; y += 1) {
       push();
@@ -37,14 +35,23 @@ function compassGrid() {
       // translate to the center of the cell
       translate(x * stepSize + stepSize / 2, y * stepSize + stepSize / 2);
 
-      const t = noise(x * 0.02, y * 0.02, frameCount * 0.01);
+      // Step 5: Conroll angle of the compass with noise
+      const factor = 0.02;
+      // Step 6: Use the mouse position to control the noise factor
+      const speedFactor = map(width - mouseX, 0, width, 0.01, 0.05, true);
+      const t = noise(x * factor, y * factor, frameCount * speedFactor);
       const angle = map(t, 0, 1, 0, 720);
 
       angleMode(DEGREES);
       rotate(angle);
 
-      stroke('black');
-      strokeWeight(3);
+      // Step 7: Control the stroke color with noise
+      const c = lerpColor(color('magenta'), color('blue'), t);
+      stroke(c);
+
+      // Step 7: Control the stroke weight with noise
+      const weight = map(t, 0, 1, 1, 5);
+      strokeWeight(weight);
 
       line(0, 0, 0, -stepSize);
       pop();
