c515c3325d3dbb6aac81f3a99d021d12b4409646
Dzmitry Plashchynski <plashchynski@gmail.com>
2023-02-05 19:52:29 +0300

    Step 3 blur

--- instagramFilter/sketch.js
+++ instagramFilter/sketch.js
@@ -104,13 +104,72 @@ function darkCorners(img) {
   return img;
 }
 
+// Convolution function taken from the course material
+function convolution(x, y, matrix, matrixSize, img) {
+  var totalRed = 0;
+  var totalGreen = 0;
+  var totalBlue = 0;
+
+  var offset = floor(matrixSize / 2);
+
+  for (var i = 0; i < matrixSize; i++) {
+    for (var j = 0; j < matrixSize; j++) {
+      var xloc = x + i - offset;
+      var yloc = y + j - offset;
+
+      var index = (img.width * yloc + xloc) * 4;
+
+      index = constrain(index, 0, img.pixels.length - 1);
+
+      totalRed += img.pixels[index + 0] * matrix[i][j];
+      totalGreen += img.pixels[index + 1] * matrix[i][j];
+      totalBlue += img.pixels[index + 2] * matrix[i][j];
+    }
+  }
+
+  return [totalRed, totalGreen, totalBlue];
+}
+
+// Step 3: Implementing the radial blur image filter
+// Takes an image as input and returns a filtered image
+function radialBlurFilter(img) {
+  var imgOut = createImage(img.width, img.height);
+  var matrixSize = matrix.length;
+
+  img.loadPixels();
+  imgOut.loadPixels();
+
+  for (var x = 0; x < img.width; x++) {
+    for (var y = 0; y < img.height; y++) {
+      var i = (y * img.width + x) * 4;
+
+      const r = img.pixels[i];
+      const g = img.pixels[i + 1];
+      const b = img.pixels[i + 2];
+      const a = img.pixels[i + 3]; // alpha channel
+
+      var c = convolution(x, y, matrix, matrixSize, img);
+
+      let dynBlur = constrain(map(dist(x, y, mouseX, mouseY), 100, 300, 0, 1), 0, 1);
+
+      imgOut.pixels[i + 0] = c[0] * dynBlur + r * (1 - dynBlur);
+      imgOut.pixels[i + 1] = c[1] * dynBlur + g * (1 - dynBlur);
+      imgOut.pixels[i + 2] = c[2] * dynBlur + b * (1 - dynBlur);
+      imgOut.pixels[i + 3] = a;
+    }
+  }
+
+  imgOut.updatePixels();
+  return imgOut;
+}
+
 // Implementing the Early Bird image filter
 // Takes an image as input and returns a filtered image
 function earlyBirdFilter(img) {
-  let resultImg;
-  resultImg = sepiaFilter(img);
+  let resultImg = img.get();
+  resultImg = sepiaFilter(resultImg);
   resultImg = darkCorners(resultImg);
-  // resultImg = radialBlurFilter(resultImg);
+  resultImg = radialBlurFilter(resultImg);
   // resultImg = borderFilter(resultImg)
   return resultImg;
 }
